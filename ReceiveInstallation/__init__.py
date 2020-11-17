3a3e4966e599b626acd14e78809f394d127bed9e
Yuki Hattori <yuki.hattori@outlook.com>
2020-11-17 22:46:59 +0900

    Added cosmosdb binding

--- ReceiveInstallation/__init__.py
+++ ReceiveInstallation/__init__.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # Azure Function Related Library
 import logging
@@ -7,7 +8,7 @@ import azure.functions as func
 # Common Library
 from lib import github_token_client
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
+def main(req: func.HttpRequest, outdoc: func.Out[func.Document]) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     installation_id = req.params.get('installation_id')
@@ -20,13 +21,29 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             installation_id = req_body.get('installation_id')
 
     if installation_id:
-        app_id = os.environ["gh_app_id"]
-        app_pem = os.environ["gh_app_pem"]
-        gh_token_client = github_token_client.GitHubTokenClient(app_id, app_pem, installation_id)
+        try: 
+            app_id = os.environ["gh_app_id"]
+            app_pem = os.environ["gh_app_pem"]
+            gh_token_client = github_token_client.GitHubTokenClient(app_id, app_pem, installation_id)
 
-        token = gh_token_client.get_token()
+            token = gh_token_client.get_token()
 
-        return func.HttpResponse(f"Your token is { token }. This HTTP triggered function executed successfully.")
+            # cosmosdb 
+            outdata = {"token": token}
+            outdoc.set(func.Document.from_json(json.dumps(outdata)))
+
+        except: 
+            logging.info(sys.exc_info())
+            return func.HttpResponse(
+                "Not Successful",
+                status_code=500
+            )
+        else:
+            return func.HttpResponse(
+                f"Your token is { token } !",
+                status_code=200
+            )
+
 
     else:
         return func.HttpResponse(
