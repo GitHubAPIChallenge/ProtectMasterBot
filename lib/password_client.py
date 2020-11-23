fa8487a90bdf3986b2911e3473fca566932a31b7
Yuki Hattori <yuki.hattori@outlook.com>
2020-11-23 17:57:40 +0900

    Added password_client test

--- lib/password_client.py
+++ lib/password_client.py
@@ -13,7 +13,6 @@ import bcrypt
 def generate_password():
     password = uuid.uuid4().hex
     hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt(12)).decode('utf-8') # for db
-    print(hash)
     return {
         "password": password, # will be given_password
         "hash": hash # will be saved_password
